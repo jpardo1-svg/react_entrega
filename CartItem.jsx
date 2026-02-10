dc2d3637251e41c19ed40c38e98fa8cf0112de47
jpardo1-svg <j.pardo1@lanuevaamerica.edu.co>
2026-02-09 20:32:45 -0500

    Enhance CartItem component with item removal and subtotal
    Added functionality to remove items and calculate subtotal for each cart item.

--- CartItem.jsx
+++ CartItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
+import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
@@ -16,18 +17,45 @@ const CartItem = ({ onContinueShopping }) => {
 
   const handleDecrement = (item) => {
     if (item.quantity > 1) {
-        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
     } else {
-        dispatch(removeItem(item.name));
+      dispatch(removeItem(item.name));
     }
   };
 
+  const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
+  };
+
+  const calculateTotalCost = (item) => {
+    return parseFloat(item.cost.substring(1)) * item.quantity;
+  };
+
   return (
-    <div>
-      <h2>Total Carrito: ${calculateTotalAmount()}</h2>
-      {/* Mapeo de items con botones de incrementar, decrementar y eliminar */}
-      <button onClick={() => alert('Próximamente')}>Checkout</button>
-      <button onClick={onContinueShopping}>Continuar Comprando</button>
+    <div className="cart-container">
+      <h2 style={{ color: 'black' }}>Total del Carrito: ${calculateTotalAmount()}</h2>
+      <div>
+        {cart.map(item => (
+          <div className="cart-item" key={item.name}>
+            <img className="cart-item-image" src={item.image} alt={item.name} />
+            <div className="cart-item-details">
+              <div className="cart-item-name">{item.name}</div>
+              <div className="cart-item-cost">Precio: {item.cost}</div>
+              <div className="cart-item-quantity">
+                <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
+                <span className="cart-item-quantity-value">{item.quantity}</span>
+                <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
+              </div>
+              <div className="cart-item-total">Subtotal: ${calculateTotalCost(item)}</div>
+              <button className="cart-item-delete" onClick={() => handleRemove(item)}>Eliminar</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="continue_shopping_btn">
+        <button className="get-started-button" onClick={(e) => onContinueShopping(e)}>Continuar Comprando</button>
+        <button className="get-started-button" onClick={() => alert('Próximamente')}>Pagar</button>
+      </div>
     </div>
   );
 };
